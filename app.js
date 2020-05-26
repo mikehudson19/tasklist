1e25dc3f7f1772586cf6a15713eefd8f7286e31c
Admin <admin@Michaels-MacBook-Pro.local>
2020-05-26 16:45:07 +0200

    change button to submit button on html and add move all from today to all functionality

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const input = document.querySelector('input');
-const button = document.querySelector('#button');
+const form = document.querySelector('form');
 const allUl = document.querySelectorAll('ul')[0];
 const todayUl = document.querySelectorAll('ul')[1];
 const completeUl = document.querySelectorAll('ul')[2];
 const clearBtn = document.querySelector('.btn-clear');
+const moveBtn = document.querySelector('.btn-move');
 
-button.addEventListener('click', taskAdd);
+form.addEventListener('submit', taskAdd);
 document.body.addEventListener('click', taskDelete);
 document.body.addEventListener('click', taskToday);
 document.body.addEventListener('click', taskComplete);
 clearBtn.addEventListener('click', clearAll);
+moveBtn.addEventListener('click', moveAll);
+
 
 // ADD TASK FUNCTION 
-function taskAdd () {
+function taskAdd (event) {
   if (!input.value) {
     alert('Please enter a task!')
   } else {
-  const listItem = document.createElement('li');
-  listItem.innerHTML = `${input.value} <i class="far fa-calendar-plus"></i> <i class="fas fa-check"></i> <i class="fas fa-trash-alt"></i> `;
-  allUl.appendChild(listItem);
-}
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${input.value} <i class="far fa-calendar-plus"></i> <i class="fas fa-check"></i> <i class="fas fa-trash-alt"></i> `;
+    allUl.appendChild(listItem);
+  }
   input.value = '';
+  event.preventDefault();
 }
 
 // DELETE TASK FUNCTION 
@@ -48,14 +52,22 @@ function taskComplete (e) {
 // CLEAR ALL TASKS 
 function clearAll () {
   const allTasks = allUl.querySelectorAll('li'); 
-  // for (let i = allTasks.length - 1; i >= 0; i--) {
-    
-  // }
   let n = allTasks.length;
   while (n > 0) {
     n-- 
   allTasks[n].remove();
 }
+}
+
+// MOVE ALL TASKS BACK TO ALL
+function moveAll () {
+  const todayTasks = document.querySelectorAll('li');
+  let n = todayTasks.length;
+  while (n > 0) {
+    n-- 
+    todayTasks[n].remove();
+    allUl.appendChild(todayTasks[n]);
+  }
 }
 
 
