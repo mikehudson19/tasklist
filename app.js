fc14523fc535573eff59a4fe1823c3c066a4e04b
Admin <admin@Michaels-MacBook-Pro.local>
2020-05-26 09:41:24 +0200

    add font awesome link and delete task functionality

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const input = document.querySelector('input');
 const button = document.querySelector('#button');
 
-button.addEventListener('click', addTask);
+button.addEventListener('click', taskAdd);
+document.body.addEventListener('click', taskDelete)
 
-function addTask () {
+function taskAdd () {
   if (!input.value) {
     alert('Please enter a task!')
   } else {
   const listItem = document.createElement('li');
-  listItem.innerText = input.value;
+  listItem.innerHTML = `${input.value} <i class="fas fa-check"></i> <i class="fas fa-trash-alt"></i> `;
   const allUl = document.querySelectorAll('ul')[0];
   allUl.appendChild(listItem);
 }
   input.value = '';
 }
+
+
+
+// DELETE TASK FUNCTION 
+function taskDelete (e) {
+  if (e.target.className.includes('fa-trash')) {
+    e.target.parentElement.remove();
+  } 
+}
+
+
+
+
